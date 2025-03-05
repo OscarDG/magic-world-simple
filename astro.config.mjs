cbdd8f08151ffb0de5c6565cd2b6caaecb923956
OscarDG <oscard.guerrerom@outlook.com>
2025-03-05 18:14:21 -0500

    updated files path alias for using @. Updated styles for header logo, activities card size, and testimonies card size

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import alias from '@rollup/plugin-alias'
 
 // https://astro.build/config
 export default defineConfig({
         experimental:{
             svg: true,
+        },
+        vite: {
+            plugins: [
+                alias({
+                    entries: [
+                        { find: '@', replacement: new URL('./src', import.meta.url).pathname }
+                    ]
+                })
+            ]
         },
 
         site: "https://OscarDG.github.io",
